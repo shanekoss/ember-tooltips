60cc58e785d9ab21c1e814d6b38bcee746405811
Shane Koss <shane@shanekoss.com>
2018-11-08 13:44:04 -0500

    catch is destroying and avoid assertion

--- addon/components/ember-popover.js
+++ addon/components/ember-popover.js
@@ -1,27 +1,24 @@
-import Ember from 'ember';
-import EmberTooltipBase from 'ember-tooltips/components/ember-tooltip-base';
+import Ember from "ember";
+import EmberTooltipBase from "ember-tooltips/components/ember-tooltip-base";
 
-const {
-  computed,
-  run,
-} = Ember;
+const { computed, run } = Ember;
 
 export default EmberTooltipBase.extend({
   popoverHideDelay: 250,
-  tooltipClassName: 'ember-popover',
+  tooltipClassName: "ember-popover",
 
-  hideDelay: computed.deprecatingAlias('popoverHideDelay', {
-    id: 'EmberTooltipBase.popoverHideDelay',
-    until: '3.2.0',
+  hideDelay: computed.deprecatingAlias("popoverHideDelay", {
+    id: "EmberTooltipBase.popoverHideDelay",
+    until: "3.2.0"
   }),
 
   _isMouseInside: false,
 
   actions: {
     hide() {
-      this.set('_isMouseInside', false);
+      this.set("_isMouseInside", false);
       this.hide();
-    },
+    }
   },
 
   addTargetEventListeners() {
@@ -34,80 +31,103 @@ export default EmberTooltipBase.extend({
 
     /* If the user clicks outside the popover, hide the popover. */
 
-    this._addEventListener('click', () => {
-      if (!this.get('_isMouseInside')) {
-        this.hide();
-      }
-    }, document);
+    this._addEventListener(
+      "click",
+      () => {
+        if (!this.get("_isMouseInside")) {
+          this.hide();
+        }
+      },
+      document
+    );
   },
 
   addPopoverTargetEventListeners() {
-
     /* We must use mouseover because it correctly
     registers hover interactivity when spacing='0'
     */
 
-    this._addEventListener('mouseenter', () => {
-      this.set('_isMouseInside', true);
+    this._addEventListener("mouseenter", () => {
+      if (this.get("isDestroying")) {
+        return;
+      }
+      this.set("_isMouseInside", true);
     });
 
-    this._addEventListener('mouseleave', () => {
-      this.set('_isMouseInside', false);
+    this._addEventListener("mouseleave", () => {
+      if (this.get("isDestroying")) {
+        return;
+      }
+      this.set("_isMouseInside", false);
     });
 
-    this._addEventListener('focusout', () => {
-      if (!this.get('_isMouseInside')) {
+    this._addEventListener("focusout", () => {
+      if (this.get("isDestroying")) {
+        return;
+      }
+      if (!this.get("_isMouseInside")) {
         this.hide();
       }
     });
   },
 
   addPopoverEventListeners() {
-    const _tooltip = this.get('_tooltip');
+    const _tooltip = this.get("_tooltip");
     const popover = _tooltip.popperInstance.popper;
 
     /* We must use mouseover because it correctly
     registers hover interactivity when spacing='0'
     */
 
-    this._addEventListener('mouseenter', () => {
-      this.set('_isMouseInside', true);
+    this._addEventListener(
+      "mouseenter",
+      () => {
+        this.set("_isMouseInside", true);
 
-      if (this.get('showOn') === 'mouseenter' && !this.get('isShown')) {
-        this.show();
-      }
-    }, popover);
+        if (this.get("showOn") === "mouseenter" && !this.get("isShown")) {
+          this.show();
+        }
+      },
+      popover
+    );
 
-    this._addEventListener('mouseleave', () => {
-      this.set('_isMouseInside', false);
+    this._addEventListener(
+      "mouseleave",
+      () => {
+        this.set("_isMouseInside", false);
 
-      if (this.get('hideOn') === 'mouseleave' && this.get('isShown')) {
-        this.hide();
-      }
-    }, popover);
+        if (this.get("hideOn") === "mouseleave" && this.get("isShown")) {
+          this.hide();
+        }
+      },
+      popover
+    );
 
-    this._addEventListener('focusout', () => {
-      if (!this.get('_isMouseInside')) {
-        this.hide();
-      }
-    }, popover);
+    this._addEventListener(
+      "focusout",
+      () => {
+        if (!this.get("_isMouseInside")) {
+          this.hide();
+        }
+      },
+      popover
+    );
   },
 
   hide() {
-    if (this.get('isDestroying')) {
+    if (this.get("isDestroying")) {
       return;
     }
 
     /* If the tooltip is about to be showed by
     a delay, stop is being shown. */
 
-    run.cancel(this.get('_showTimer'));
+    run.cancel(this.get("_showTimer"));
 
     run.later(() => {
-      if (!this.get('_isMouseInside')) {
+      if (!this.get("_isMouseInside")) {
         this._hideTooltip();
       }
-    }, +this.get('popoverHideDelay'));
-  },
-
+    }, +this.get("popoverHideDelay"));
+  }
 });
